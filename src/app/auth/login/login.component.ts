0a8aa326eb121722d16f83f0d7e34d8f81155fd3
Jyoti Pathania <jyoti.pathania@pb.com>
2023-07-08 00:25:30 +0530

    The Store Service API - Implementing the Login Screen and Defining NgRx Actions using Action Creators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,6 +7,8 @@ import {AuthService} from "../auth.service";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
+import { AppState } from '../reducers';
+import { login } from '../auth.action';
 
 @Component({
   selector: 'login',
@@ -20,8 +22,9 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
-
+      private router:Router,
+      private store: Store<AppState>
+      ) {        
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
           password: ['test', [Validators.required]]
@@ -34,7 +37,25 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+      const val = this.form.value;
+      this.auth.login(val.email, val.password).pipe(
+        tap((user)=>{
+          console.log(user);
+         /* this.store.dispatch({
+            type: "Login Page",
+            payload: {
+              user // or user: user
+            }
+          })*/
 
+          this.store.dispatch(login({user}))
+          this.router.navigateByUrl("/courses");
+        })
+      ).subscribe(
+        noop,
+        ()=> alert("login failed")
+        
+      )
   }
 
 }
